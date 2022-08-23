5860823180c28f42bb2263e7b2e35e29562693a8
RokHyung Son <rokong.son@kidsnote.com>
2022-08-23 13:44:16 +0900

    feat(folder): Folder 앱 추가

--- configs/settings.py
+++ configs/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "treebeard",
+    "treebeard_folder",
 ]
 
 
---
+++ treebeard_folder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TreebeardFolderConfig(AppConfig):
+    name = "treebeard_folder"
